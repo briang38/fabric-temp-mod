a831413de60228441adf3b449e2ef1c0c368b81f
briang387 <briangutt.official@gmail.com>
2026-02-10 00:27:47 -0600

    added land and the png textures for pink garnet and raw pink garnet

--- src/main/java/net/brian/tutorialmod/TutorialMod.java
+++ src/main/java/net/brian/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.brian.tutorialmod;
 
+import net.brian.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,6 +13,7 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		
+		ModItems.registerModItems();
 
 		LOGGER.info("Hello Fabric world!");
 	}
