babda3d20748cc2f27f26b3c7f292c628b138e86
briang387 <briangutt.official@gmail.com>
2026-02-10 00:07:29 -0600

    added mod items java class as well as package

---
+++ src/main/java/net/brian/tutorialmod/TutorialModClient.java
@@ -0,0 +1,11 @@
+package net.brian.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client-side initialization code goes here
+    }
+
+}
