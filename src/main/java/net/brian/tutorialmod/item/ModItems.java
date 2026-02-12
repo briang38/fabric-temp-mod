dc92451b0c5be1d37abaf68ef88d0c140c316595
briang38 <brianmgutt@lewisu.edu>
2026-02-11 19:28:17 -0600

    fixed titianium ingot implementation

--- src/main/java/net/brian/tutorialmod/item/ModItems.java
+++ src/main/java/net/brian/tutorialmod/item/ModItems.java
@@ -11,6 +11,7 @@ import net.minecraft.util.Identifier;
 public class ModItems {
 
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item TITANIUM_INGOT = registerItem("titanium_ingot", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
@@ -21,6 +22,7 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(PINK_GARNET);
+            entries.add(TITANIUM_INGOT);
         });
     }
 
