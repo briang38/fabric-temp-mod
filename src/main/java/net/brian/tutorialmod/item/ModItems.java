c9fd29d674cdc9ba962b66113f5f615fe5550ad6
briang38 <brianmgutt@lewisu.edu>
2026-02-12 14:34:26 -0600

    added raw titanium

--- src/main/java/net/brian/tutorialmod/item/ModItems.java
+++ src/main/java/net/brian/tutorialmod/item/ModItems.java
@@ -13,6 +13,7 @@ public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item TITANIUM_INGOT = registerItem("titanium_ingot", new Item(new Item.Settings()));
     public static final Item PLASTEEL_INGOT = registerItem("plasteel_ingot", new Item(new Item.Settings()));
+    public static final Item RAW_TITANIUM = registerItem("raw_titanium", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
@@ -25,6 +26,7 @@ public class ModItems {
             entries.add(PINK_GARNET);
             entries.add(TITANIUM_INGOT);
             entries.add(PLASTEEL_INGOT);
+            entries.add(RAW_TITANIUM);
         });
     }
 
