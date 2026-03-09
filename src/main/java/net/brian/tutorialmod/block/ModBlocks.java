fa53f006b86e10fa672d1c38fbefbcdb38422849
briang38 <brianmgutt@lewisu.edu>
2026-03-09 16:33:38 -0500

    adding changes

--- src/main/java/net/brian/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/brian/tutorialmod/block/ModBlocks.java
@@ -15,7 +15,7 @@ import net.minecraft.util.Identifier;
 public class ModBlocks {
 
     //This adds the blocks to the game, but they won't show up in the creative inventory until we add them to an item group using the ItemGroupEvents API.
-    public static final Block PINK_GARENT_BLOCK = registerBlock("pink_garnet_block", 
+    public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block", 
             new Block(AbstractBlock.Settings.create().strength(4f)
                 .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
@@ -28,7 +28,7 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.create().strength(3f)
                 .requiresTool().sounds(BlockSoundGroup.NETHER_BRICKS)));
 
-    public static final Block TITAINIUM_ORE = registerBlock("titanium_ore", 
+    public static final Block TITANIUM_ORE = registerBlock("titanium_ore", 
             new Block(AbstractBlock.Settings.create().strength(3f)
                 .requiresTool().sounds(BlockSoundGroup.STONE)));
 
@@ -47,10 +47,10 @@ public class ModBlocks {
         TutorialMod.LOGGER.info("Registering Mod Blocks for " + TutorialMod.MOD_ID);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
-            entries.add(PINK_GARENT_BLOCK);
-            entries.add(TITANIUM_BLOCK);
-            entries.add(RAW_TITANIUM_BLOCK);
-            entries.add(TITAINIUM_ORE);
+            entries.add(ModBlocks.PINK_GARNET_BLOCK);
+            entries.add(ModBlocks.TITANIUM_BLOCK);
+            entries.add(ModBlocks.RAW_TITANIUM_BLOCK);
+            entries.add(ModBlocks.TITANIUM_ORE);
         });
     }
 }
